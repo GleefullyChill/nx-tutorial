df22c76026b9e49ce7f46283c24e7950963adb2b
GleefullyChill <stephan.lofstrom@gmail.com>
2021-11-16 11:03:19 -0700

    add button test

--- tutorial/apps/tutorial-e2e/src/integration/app.spec.ts
+++ tutorial/apps/tutorial-e2e/src/integration/app.spec.ts
@@ -11,3 +11,14 @@ describe('tutorial', () => {
     getGreeting().contains('Welcome to tutorial!');
   });
 });
+import { getAddTodoButton, getTodos } from '../support/app.po';
+
+describe('TodoApps', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display todos', () => {
+    getTodos().should((t) => expect(t.length).equal(2));
+    getAddTodoButton().click();
+    getTodos().should((t) => expect(t.length).equal(3));
+  });
+});
